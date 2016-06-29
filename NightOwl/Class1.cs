a647c1dc35808ad0a042934a8ddb01cbff9d6201
janavarro95 <janavarro95@users.noreply.github.com>
2016-06-29 10:32:42 -0700

    Add files via upload

--- NightOwl/Class1.cs
+++ NightOwl/Class1.cs
@@ -16,8 +16,6 @@ Issues:
         -transition still doesnt work. However atleast it is dark now.
 
 -Known glitched
-    -player passes out when going to bed
-    -money not being propperly calculated after shipments
 
 */
 
@@ -31,6 +29,7 @@ namespace Stardew_NightOwl
         int player_y;
         string prior_map;
 
+        bool reset_check;
         bool was_raining;
         bool time_reset;
         int prior_money;
@@ -38,15 +37,11 @@ namespace Stardew_NightOwl
         float pre_stam;
         int pre_health;
 
-
-        int safety_cash;
-        
-
         bool lighting_transition; //if true transition happens. If false, game starts out bright at 2AM. Good to remove that awkward change from dark to bright.
         bool warp;
         bool stay_up;
 
-       // bool wipe_mail;
+        bool wipe_mail;
         bool protect_money;
         bool persistant_health;
         bool persistant_stamina;
@@ -55,14 +50,19 @@ namespace Stardew_NightOwl
 
         bool once;
         bool up_late;
-        int very_old_money;
 
+        bool first_check;
 
+        bool warped_check;
+
+        bool super_map_warping_check;
 
         public override void Entry(params object[] objects)
         {
             StardewModdingAPI.Events.TimeEvents.TimeOfDayChanged += TimeEvents_TimeOfDayChanged;
-            StardewModdingAPI.Events.TimeEvents.DayOfMonthChanged += TimeEvents_DayOfMonthChanged;
+            // StardewModdingAPI.Events.TimeEvents.DayOfMonthChanged += TimeEvents_DayOfMonthChanged;
+            StardewModdingAPI.Events.TimeEvents.OnNewDay += TimeEvents_DayOfMonthChanged;
+
             StardewModdingAPI.Events.PlayerEvents.LoadedGame += PlayerEvents_LoadedGame;
             StardewModdingAPI.Events.GameEvents.FourthUpdateTick += GameEvents_FourthUpdateTick;
         }
@@ -76,12 +76,21 @@ namespace Stardew_NightOwl
                 if (once==true)
                 {
                   //  Log.AsyncM("NEW MONEY" + Game1.player.money);
-                    Game1.player.money = Game1.player.money + post_money;
+                   // Game1.player.money = Game1.player.money + post_money;
                     once = false;
                 }
 
             }
-
+            if (warped_check == false)
+            {
+                if (warp == true)
+                {
+                    Log.Info("Warping!!!");
+                    Game1.warpFarmer(prior_map, player_x, player_y, false);
+                    warped_check = true;
+                    super_map_warping_check = true;
+                }
+                }
 
         }
 
@@ -92,16 +101,36 @@ namespace Stardew_NightOwl
             game_loaded = true;
             once = false;
             up_late = false;
+            first_check = false;
+            warped_check = true;
+            super_map_warping_check = true;
         }
 
-        public void TimeEvents_DayOfMonthChanged(object sender, StardewModdingAPI.Events.EventArgsIntChanged e)
+        public void TimeEvents_DayOfMonthChanged(object sender, StardewModdingAPI.Events.EventArgsNewDay e)
         {
             if (game_loaded == false) return;
+            if (first_check == false)
+            {
+                first_check = true;
+                Log.Info("first");
+                return;
+            }
+            else
+            {
+                first_check = false;
+                Log.Info("Second");
+            }
+            Game1.farmerShouldPassOut = false; //make the farmer collapse.
+           
+            reset_check = false;
+            //Game1.farmerShouldPassOut = false; //make the farmer collapse.
             DataLoader();
             MyWritter();
             //List<string> newmail = new List<string>();
+            Queue<string> newmail = new Queue<string>();
+           
             up_late = false;
-
+            
             if (time_reset == true)
             {
                 was_raining = false;
@@ -110,10 +139,9 @@ namespace Stardew_NightOwl
                 //post_money = Game1.player.money;
                 if (protect_money == true) {
 
-                    //grab the amount subtracted
+                    Game1.player.money += post_money;
                     once = true;
-                    
-                   // Log.AsyncM("OLD MONEY:" + very_old_money);
+                
 
                 } //add the money back from colapsing.
                 time_reset = false; //reset my bool.
@@ -122,8 +150,6 @@ namespace Stardew_NightOwl
                 //Game1.player.money += very_old_money;
             }
 
-      
-            /*
             if (wipe_mail == true)
             {
                 foreach (var i in Game1.mailbox) //delete those annoying charge messages. If only I could do this with mail IRL.
@@ -135,20 +161,16 @@ namespace Stardew_NightOwl
                         Log.Info("Found bad mail");
                     }
                     else {
-                        newmail.Add(i);
+                        newmail.Enqueue(i);
                     }
                 }
                 Game1.mailbox.Clear();
                 foreach (string mail in newmail)
                 {
-                    Game1.player.mailReceived.Remove(mail);
-                    Game1.player.mailForTomorrow.Add(mail);
-                    //Game1.addMailForTomorrow(mail, false, false);
+                    Game1.mailbox.Enqueue(mail);
                 }
             }
-            */
-
-
+            warped_check = false;
         }
 
 
@@ -157,7 +179,7 @@ namespace Stardew_NightOwl
             if (game_loaded == false) return;
             float color_mod;
             //transition the lighting for more "realistic" lighting from 2am to 11am. There is an awkward screen shuffle that happens because of this update.
-
+          
             if (lighting_transition == true)
             {
                 if (Game1.timeOfDay > 200 && Game1.timeOfDay < 1100)
@@ -184,40 +206,52 @@ namespace Stardew_NightOwl
             }
 
 
-            if (Game1.timeOfDay == 550)
+            if (Game1.timeOfDay == 550) //if the game time is 5:50 AM
             {
                 up_late = true;
             }
 
-            if (Game1.timeOfDay == 600)
+            if (Game1.timeOfDay == 600) //when time is 6:00 AM after staying up
             {
+
                 if (up_late == false)
                 {
-                        return;
+                    return;
                 }
 
-                //if (time_reset == false)  return;
-                time_reset = true;
-                Game1.farmerShouldPassOut = true; //make the farmer collapse.
-                player_x = Game1.player.getTileX();
-                player_y = Game1.player.getTileY();
-                prior_map = Game1.player.currentLocation.name;
-                pre_stam = Game1.player.stamina;
-                pre_health = Game1.player.health;
-                prior_money = Game1.player.money;
+     
+                if (super_map_warping_check == true)
+                {
+                    if (Game1.currentMinigame != null) Game1.currentMinigame = null;
+                    //if (time_reset == false)  return;
+                    time_reset = true;
+                    player_x = Game1.player.getTileX();
+                    player_y = Game1.player.getTileY();
+                    prior_map = Game1.player.currentLocation.name;
 
-                if (Game1.player.money <= 10000)
-                {
-                    post_money = prior_money / 10;
-                }
-                else
-                {
-                    post_money = 1000;
+                    Log.Info(prior_map);
+                    Log.Info(player_x);
+                    Log.Info(player_y);
+                    super_map_warping_check = false;
+
+
+                    Game1.farmerShouldPassOut = true; //make the farmer collapse.
+
+                    pre_stam = Game1.player.stamina;
+                    pre_health = Game1.player.health;
+                    prior_money = Game1.player.money;
+
+                    if (Game1.player.money <= 10000)
+                    {
+                        post_money = prior_money / 10;
+                    }
+                    else
+                    {
+                        post_money = 1000;
+                    }
                 }
             }
-
-           
-            }
+}
         
 
         void MyWritter()
@@ -230,70 +264,46 @@ namespace Stardew_NightOwl
             string[] mystring3 = new string[20];
             if (!File.Exists(mylocation3))
             {
-                Console.WriteLine("The data file for Night Owl wasn't found. Time to create it!");
-
-                //write out the info to a text file at the end of a day. This will run if it doesnt exist.
-
+              Log.Info("The data file for Night Owl wasn't found. Time to create it!");
                 mystring3[0] = "Player: Night Owl Config. Feel free to edit.";
                 mystring3[1] = "====================================================================================";
-
                 mystring3[2] = "Stay up to 6 AM?: Do you wan't to stay up till 6 AM the next morning?";
                 mystring3[3] = stay_up.ToString();
-
                 mystring3[4] = "Custom Lighting Transition?: Do you want to have a lighting transition from 2AM to 5:50 AM? Setting this to false will keep the world dark until the player passes out or goes to bed.";
                 mystring3[5] = lighting_transition.ToString();
-
                 mystring3[6] = "Warp to collapse position?: True: Stay in the same place. False: Warp back home.";
                 mystring3[7] = warp.ToString();
-
-                
-               // mystring3[8] = "Clean out charges mail?: Get rid of the annoying We charged X gold for your health fees, etc.";
-              //  mystring3[9] = wipe_mail.ToString();
-
                 mystring3[8] = "Keep Money?: True: Don't be charged for passing out.";
                 mystring3[9] = protect_money.ToString();
-                
                 mystring3[10] = "Keep stamina when staying up? When the farmer passes out at 6, should their stamina be their stamina before collapsing?";
                 mystring3[11] = persistant_stamina.ToString();
                 mystring3[12] = "Keep health when staying up? When the farmer passes out at 6, should their health be their health before collapsing?";
                 mystring3[13] = persistant_health.ToString();
-
-                /*
-                mystring3[17] = "Nightly Stamina Value: This is the value of the player's stamina that was saved when the player slept.";
-                mystring3[18] = nightly_stamina_value.ToString(); //this should save the player's stamina upon sleeping.
-                */
-
+                mystring3[14] = "Clean out charges mail?: Get rid of the annoying We charged X gold for your health fees, etc.";
+                mystring3[15] = wipe_mail.ToString();
                 File.WriteAllLines(mylocation3, mystring3);
             }
 
             else
             {
                 //    Console.WriteLine("HEY IM SAVING DATA");
-
                 //write out the info to a text file at the end of a day.
                 mystring3[0] = "Player: Night Owl Config. Feel free to edit.";
                 mystring3[1] = "====================================================================================";
-
                 mystring3[2] = "Stay up to 6 AM?: Do you wan't to stay up till 6 AM the next morning?";
                 mystring3[3] = stay_up.ToString();
-
                 mystring3[4] = "Custom Lighting Transition?: Do you want to have a lighting transition from 2AM to 5:50 AM? Setting this to false will keep the world dark until the player passes out or goes to bed.";
                 mystring3[5] = lighting_transition.ToString();
-
                 mystring3[6] = "Warp to collapse position?: True: Stay in the same place. False: Warp back home.";
                 mystring3[7] = warp.ToString();
-
-
-                // mystring3[8] = "Clean out charges mail?: Get rid of the annoying We charged X gold for your health fees, etc.";
-                //mystring3[9] = wipe_mail.ToString();
-
                 mystring3[8] = "Keep Money?: True: Don't be charged for passing out.";
                 mystring3[9] = protect_money.ToString();
-
                 mystring3[10] = "Keep stamina when staying up? When the farmer passes out at 6, should their stamina be their stamina before collapsing?";
                 mystring3[11] = persistant_stamina.ToString();
                 mystring3[12] = "Keep health when staying up? When the farmer passes out at 6, should their health be their health before collapsing?";
                 mystring3[13] = persistant_health.ToString();
+                mystring3[14] = "Clean out charges mail?: Get rid of the annoying We charged X gold for your health fees, etc.";
+                mystring3[15] = wipe_mail.ToString();
 
                 File.WriteAllLines(mylocation3, mystring3);
             }
@@ -307,37 +317,29 @@ namespace Stardew_NightOwl
             string mylocation3 = mylocation + ".txt";
             if (!File.Exists(mylocation3)) //if not data.json exists, initialize the data variables to the ModConfig data. I.E. starting out.
             {
-                Console.WriteLine("Loading Night_Owl_Config");
+               Log.Info("Loading Night_Owl_Config");
                 lighting_transition = true;
                 warp = true;
                 stay_up = true;
 
                 persistant_health = true;
                 persistant_stamina = true;
-                //wipe_mail = true;
+                wipe_mail = true;
                 protect_money = true;
 
             }
 
             else
             {
-                //        Console.WriteLine("HEY THERE IM LOADING DATA");
-
-                //loads the BuildEndurance_data upon loading the mod
                 string[] readtext = File.ReadAllLines(mylocation3);
-                stay_up = Convert.ToBoolean(readtext[3]);
-                lighting_transition = Convert.ToBoolean(readtext[5]);  //these array locations refer to the lines in BuildEndurance_data.json
-                warp = Convert.ToBoolean(readtext[7]);
-
-
-                //wipe_mail = Convert.ToBoolean(readtext[9]);
-                protect_money = Convert.ToBoolean(readtext[9]);
-                persistant_stamina = Convert.ToBoolean(readtext[11]);
-                persistant_health = Convert.ToBoolean(readtext[13]);
-
-
+                stay_up = Convert.ToBoolean(readtext[3]);//will the player stay up?
+                lighting_transition = Convert.ToBoolean(readtext[5]);  //does the lighting transition occur?
+                warp = Convert.ToBoolean(readtext[7]); //does the player warp back to the spot they passed out at?
+                protect_money = Convert.ToBoolean(readtext[9]); //Is their money safe from stealing?
+                persistant_stamina = Convert.ToBoolean(readtext[11]); //Does the player have the same stam when collapsing?
+                persistant_health = Convert.ToBoolean(readtext[13]); //Ditto but with health.
+                wipe_mail = Convert.ToBoolean(readtext[15]); //will I clean out their mailbox?
             }
         }
-
     }
 }
--- NightOwl/ReadMe.txt
+++ NightOwl/ReadMe.txt
@@ -1,10 +1,3 @@
-Stardew Valley Night Owl 1.0.1
-
-Updated at 12:29 AM on 6/4/16
-
--Fixed glitch where shipments wouldn't be calculated propperly when staying up late.
--Fixed glitch where going to bed would cause you to pass out.
-
 Stardew Valley Night Owl is a mod that allows players to stay up a full 24 hours instead of being restricted to 2AM.
 Some of the features include
 -A transitional lighting effect that lasts from 2AM to 6 AM
