db9cda9e8b6ef7238c3aad4044b742fe827739e2
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-07-29 20:03:22 -0400

    refactor NoMorePets
    This commit formats/documents/simplifies code, standardises naming conventions, etc.

--- GeneralMods/NoMorePets/Class1.cs
+++ GeneralMods/NoMorePets/Class1.cs
@@ -1,41 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
 
 namespace Omegasis.NoMorePets
 {
-    public class Class1 :Mod
+    /// <summary>The mod entry point.</summary>
+    public class NoMorePets : Mod
     {
-        bool game_loaded;
+        /*********
+        ** Properties
+        *********/
+        /// <summary>Whether the player loaded a save.</summary>
+        private bool IsGameLoaded;
+
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            StardewModdingAPI.Events.GameEvents.UpdateTick += GameEvents_UpdateTick;
-            StardewModdingAPI.Events.SaveEvents.AfterLoad += PlayerEvents_LoadedGame;
+            GameEvents.UpdateTick += this.GameEvents_UpdateTick;
+            SaveEvents.AfterLoad += this.SaveEvents_AfterLoad;
         }
 
-        public void PlayerEvents_LoadedGame(object sender, EventArgs e)
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>The method invoked after the player loads a save.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        public void SaveEvents_AfterLoad(object sender, EventArgs e)
         {
-            game_loaded = true;
+            this.IsGameLoaded = true;
         }
 
-
+        /// <summary>The method invoked when the game updates (roughly 60 times per second).</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
         public void GameEvents_UpdateTick(object sender, EventArgs e)
         {
-            if (game_loaded == false) return;
-            List<NPC> my_npc_list = new List<NPC>();
-            if (Game1.player == null) return;
-            string pet_name = Game1.player.getPetName();
-            if (Game1.player.currentLocation.name == "Farm")
+            if (!this.IsGameLoaded || Game1.player == null)
+                return;
+
+            string petName = Game1.player.getPetName();
+            if (Game1.player.currentLocation is Farm)
             {
-                foreach(NPC npc in Game1.player.currentLocation.characters)
+                foreach (NPC npc in Game1.player.currentLocation.characters.ToArray())
                 {
-                  
-                    if (npc.name == pet_name) my_npc_list.Add(npc);
-                }
-               foreach(var location in Game1.locations)
-                {
-                    if (location.name == "Farm" || location.name == "farm") StardewValley.Game1.removeCharacterFromItsLocation(pet_name);
+                    if (npc.name == petName)
+                        Game1.removeCharacterFromItsLocation(petName);
                 }
             }
         }
